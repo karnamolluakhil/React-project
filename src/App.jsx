b580cce31f369ddb54d3a27b1e342a1b1f04c837
karnamolluakhil <karunamakhil@gmail.com>
2025-02-24 14:53:08 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import LandingPage from "./components/LandingPage.jsx";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
